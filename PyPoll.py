6965b5b12b3dd2505e866220c64de1ef8d5aa605
eoweed <90293383+eoweed@users.noreply.github.com>
2022-07-01 13:17:30 -0700

    added election winners

--- PyPoll.py
+++ PyPoll.py
@@ -21,40 +21,161 @@
 # with open(filename) as file_variable
 # make sure filename has correct slashes
 # make sure filename is in quotes
-file_to_load = 'C:/Users/EW/Desktop/Bootcamp/election-analysis/Resources/election_results.csv'
-with open(file_to_load) as election_data:
-    print(election_data)
+# file_to_load = 'C:/Users/EW/Desktop/Bootcamp/election-analysis/Resources/election_results.csv'
+# with open(file_to_load) as election_data:
+#     print(election_data)
 
 
-# import files using os method, aka. indirect method
-# os method joins a file to a folder even if you don't know the directr path
+# # import files using os method, aka. indirect method
+# # os method joins a file to a folder even if you don't know the directr path
+# import csv
+# import os
+# # dir(os)
+# file_to_load = os.path.join("Resources", "election_results.csv")
+# with open(file_to_load) as election_data:
+#     print(election_data)
+
+
+# # Module 3.4.3 write data to a file ("w" mode)
+# file_to_save = "../election-analysis/analysis/election_analysis.txt"
+# with open(file_to_save, "w") as txt_file:
+#     # write on line 1 with commas separating each word
+#     # txt_file.write("Arapahoe, ")
+#     # txt_file.write("Denver, ")
+#     # txt_file.write("Jefferson, ")
+#     # write each county on a separate line using \n
+#     # txt_file.write("\nArapahoe\nDenver\nJefferson")
+#     txt_file.write("Counties in the Election")
+#     txt_file.write("\n---------------------")
+#     txt_file.write("\nArapahoe\nDenver\nJefferson")
+
+# # Module 3.4.4 read the election results
+# import csv
+# file_to_load = "../election-analysis/Resources/election_results.csv"
+# with open(file_to_load) as election_data:
+#     # read data with reader function
+#     file_reader = csv.reader(election_data)
+#     # print all rows in the election data file
+#     # for row in file_reader:
+#     #     print(row)
+    
+#     # print header row using next function
+#     headers = next(file_reader)
+#     print(headers)
+    
+# # Module 3.5.1 calculate total number of votes cast in election
+# # re-write code above to keep everything together
+# import csv
+# # assign variable to filepath for election data that will be analyzed
+# file_to_load = "C:/Users/EW/Desktop/Bootcamp/election-analysis/Resources/election_results.csv"
+# # assign varable to filepath where analysis results will be saved
+# file_to_save = "C:/Users/EW/Desktop/Bootcamp/election-analysis/analysis/election_analysis.txt"
+# # set intial total to zero
+# total_votes = 0
+# # open file after total votes is set to zero
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     headers = next(file_reader)
+#     for row in file_reader:
+#         # same as total_votes = total_votes + 1
+#         total_votes += 1
+# print(total_votes)
+
+
+    
+# # Module 3.5.2 who are the candidates
+# # re-write code above to keep everything together
+# import csv
+# # assign variable to filepath for election data that will be analyzed
+# file_to_load = "C:/Users/EW/Desktop/Bootcamp/election-analysis/Resources/election_results.csv"
+# # assign varable to filepath where analysis results will be saved
+# file_to_save = "C:/Users/EW/Desktop/Bootcamp/election-analysis/analysis/election_analysis.txt"
+# # set intial total to zero
+# total_votes = 0
+# # create list(array) to hold candidate names
+# candidate_options = []
+# # open file after total votes is set to zero
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     headers = next(file_reader)
+#     for row in file_reader:
+#         # same as total_votes = total_votes + 1
+#         total_votes += 1
+#         # get candidate names
+#         candidate_name = row[2]
+#         if candidate_name not in candidate_options:
+#             candidate_options.append(candidate_name)
+# print(total_votes)
+# print(candidate_options)
+
+
+   
+# # Module 3.5.3 how many votes per candidate
+# # re-write code above to keep everything together
+# import csv
+# # assign variable to filepath for election data that will be analyzed
+# file_to_load = "C:/Users/EW/Desktop/Bootcamp/election-analysis/Resources/election_results.csv"
+# # assign varable to filepath where analysis results will be saved
+# file_to_save = "C:/Users/EW/Desktop/Bootcamp/election-analysis/analysis/election_analysis.txt"
+# # set intial total to zero
+# total_votes = 0
+# # create list(array) to hold candidate names
+# candidate_options = []
+# # create dictionary to hold votes per candidate
+# candidate_votes = {}
+# # open file after total votes is set to zero
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     headers = next(file_reader)
+#     for row in file_reader:
+#         # same as total_votes = total_votes + 1
+#         total_votes += 1
+#         # get candidate names
+#         candidate_name = row[2]
+#         if candidate_name not in candidate_options:
+#             candidate_options.append(candidate_name)
+#             candidate_votes[candidate_name] = 0
+#         candidate_votes[candidate_name] +=1
+# print(total_votes)
+# print(candidate_options)
+# print(candidate_votes)
+
+
+   
+# Module 3.5.4 percent votes per candidate
+# re-write code above to keep everything together
 import csv
-import os
-# dir(os)
-file_to_load = os.path.join("Resources", "election_results.csv")
+# assign variable to filepath for election data that will be analyzed
+file_to_load = "C:/Users/EW/Desktop/Bootcamp/election-analysis/Resources/election_results.csv"
+# assign varable to filepath where analysis results will be saved
+file_to_save = "C:/Users/EW/Desktop/Bootcamp/election-analysis/analysis/election_analysis.txt"
+# set intial total to zero
+total_votes = 0
+# create list(array) to hold candidate names
+candidate_options = []
+# create dictionary to hold votes per candidate {name:votes}
+candidate_votes = {}
+# open file after total votes is set to zero
 with open(file_to_load) as election_data:
-    print(election_data)
-
-
-# Module 3.4.3 write data to a file ("w" mode)
-file_to_save = "../election-analysis/analysis/election_analysis.txt"
-with open(file_to_save, "w") as txt_file:
-    # write on line 1 with commas separating each word
-    # txt_file.write("Arapahoe, ")
-    # txt_file.write("Denver, ")
-    # txt_file.write("Jefferson, ")
-    # write each county on a separate line using \n
-    # txt_file.write("\nArapahoe\nDenver\nJefferson")
-    txt_file.write("Counties in the Election")
-    txt_file.write("\n---------------------")
-    txt_file.write("\nArapahoe\nDenver\nJefferson")
-
-# Module 3.4.4 read the election results
-import csv
-file_to_load = "../election-analysis/Resources/election_results.csv"
-with open(file_to_load) as election_data:
-    # read data with reader function
     file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+
     for row in file_reader:
-        print(row)
-        
+        # same as total_votes = total_votes + 1
+        total_votes += 1
+        # get candidate names
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] +=1
+
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        print(f"{candidate_name}: recieved {vote_percentage:.2f}% of the vote.")
+
+print(total_votes)
+print(candidate_votes)
+print(candidate_options)
