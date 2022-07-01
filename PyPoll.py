41f8c361b424866ad71be34a75050a4609ea852b
eoweed <90293383+eoweed@users.noreply.github.com>
2022-07-01 08:56:37 -0700

    new python materials

---
+++ PyPoll.py
@@ -0,0 +1,24 @@
+# goals of project
+# total votes cast
+# list of candidates who recieved votes
+# total votes per candidate
+# Percent of votes per candidate
+# winner based on popular vote
+
+# Practice using datetime module
+# Import the datetime class from the datetime module
+import datetime
+# Use the now() attribut to get the present time
+now = datetime.datetime.now()
+print("The time right now is", now)
+
+# Use the csv module to read files in python
+import csv
+dir(csv)
+
+
+# Syntax for opening a file
+# with open(filename) as file_variable
+file_to_load = 
+with open(file_to_load) as election_data:
+    print(election_data)
--- Python_practice.py
+++ Python_practice.py
@@ -3,5 +3,3 @@ x = 0
 while x <= 5:
         print(x)
         x = x + 1
-        
-
