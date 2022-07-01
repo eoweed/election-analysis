7d2c775ab02aa01050284eaa366b8df61906c2df
eoweed <90293383+eoweed@users.noreply.github.com>
2022-07-01 13:55:02 -0700

    add results to text file

--- PyPoll.py
+++ PyPoll.py
@@ -142,7 +142,97 @@
 
 
    
-# Module 3.5.4 percent votes per candidate
+# # Module 3.5.4 percent votes per candidate
+# # re-write code above to keep everything together
+# import csv
+# # assign variable to filepath for election data that will be analyzed
+# file_to_load = "C:/Users/EW/Desktop/Bootcamp/election-analysis/Resources/election_results.csv"
+# # assign varable to filepath where analysis results will be saved
+# file_to_save = "C:/Users/EW/Desktop/Bootcamp/election-analysis/analysis/election_analysis.txt"
+# # set intial total to zero
+# total_votes = 0
+# # create list(array) to hold candidate names
+# candidate_options = []
+# # create dictionary to hold votes per candidate {name:votes}
+# candidate_votes = {}
+# # open file after total votes is set to zero
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     headers = next(file_reader)
+
+#     for row in file_reader:
+#         # same as total_votes = total_votes + 1
+#         total_votes += 1
+#         # get candidate names
+#         candidate_name = row[2]
+
+#         if candidate_name not in candidate_options:
+#             candidate_options.append(candidate_name)
+#             candidate_votes[candidate_name] = 0
+#         candidate_votes[candidate_name] +=1
+
+#     for candidate_name in candidate_votes:
+#         votes = candidate_votes[candidate_name]
+#         vote_percentage = float(votes) / float(total_votes) * 100
+#         print(f"{candidate_name}: recieved {vote_percentage:.2f}% of the vote.")
+
+# print(total_votes)
+# print(candidate_votes)
+# print(candidate_options)
+
+
+# # Module 3.5.5 determine winner of election
+# # re-write code above to keep everything together
+# import csv
+# # assign variable to filepath for election data that will be analyzed
+# file_to_load = "C:/Users/EW/Desktop/Bootcamp/election-analysis/Resources/election_results.csv"
+# # assign varable to filepath where analysis results will be saved
+# file_to_save = "C:/Users/EW/Desktop/Bootcamp/election-analysis/analysis/election_analysis.txt"
+# # set intial total to zero
+# total_votes = 0
+# # create list(array) to hold candidate names
+# candidate_options = []
+# # create dictionary to hold votes per candidate {name:votes}
+# candidate_votes = {}
+# # declare variables to determine who wins
+# winning_candidate = ""
+# winning_count = 0
+# winning_percentage = 0
+# # open file after total votes is set to zero
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     headers = next(file_reader)
+
+#     for row in file_reader:
+#         # same as total_votes = total_votes + 1
+#         total_votes += 1
+#         # get candidate names
+#         candidate_name = row[2]
+
+#         if candidate_name not in candidate_options:
+#             candidate_options.append(candidate_name)
+#             candidate_votes[candidate_name] = 0
+#         candidate_votes[candidate_name] +=1
+
+#     for candidate_name in candidate_votes:
+#         votes = candidate_votes[candidate_name]
+#         vote_percentage = float(votes) / float(total_votes) * 100
+
+#         if (votes > winning_count) and (vote_percentage > winning_percentage):
+#             winning_count = votes
+#             winning_percentage = vote_percentage
+#             winning_candidate = candidate_name
+#         print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+#     winning_candidate_summary = (
+#         f"----------------\n"
+#         f"Winner: {winning_candidate}\n"
+#         f"Winning Vote Count: {winning_count:<}\n"
+#         f"Winning Percentage: {winning_percentage:.1f}%\n"
+#         f"----------------\n")
+#     print(winning_candidate_summary)
+
+
+# Module 3.6.1 write results to a text file
 # re-write code above to keep everything together
 import csv
 # assign variable to filepath for election data that will be analyzed
@@ -155,6 +245,10 @@ total_votes = 0
 candidate_options = []
 # create dictionary to hold votes per candidate {name:votes}
 candidate_votes = {}
+# declare variables to determine who wins
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 # open file after total votes is set to zero
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -171,11 +265,20 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] +=1
 
-    for candidate_name in candidate_votes:
-        votes = candidate_votes[candidate_name]
-        vote_percentage = float(votes) / float(total_votes) * 100
-        print(f"{candidate_name}: recieved {vote_percentage:.2f}% of the vote.")
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes) / float(total_votes) * 100
+    print(f"\n{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-print(total_votes)
-print(candidate_votes)
-print(candidate_options)
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_candidate = candidate_name
+        winning_percentage = vote_percentage
+
+winning_candidate_summary = (
+    f"----------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:<}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"----------------\n")
+print(winning_candidate_summary)
